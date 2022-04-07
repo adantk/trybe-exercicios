edf9b777153357b8f2c6bda83920dc059afd3830
Adan Filipe - Turma 22 - B <adan.cta@gmail.com>
2022-04-07 19:17:39 -0300

    Resolvido exercicio 3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -4,10 +4,20 @@ for( let list of numbers){
     console.log(list);
 }
  */
-/* Exercicio 2 */ 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* /* Exercicio 2 */ 
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 for( let i = 0; i< numbers.length; i++){
    soma += numbers[i];
 }
-console.log(soma);
+console.log(soma); */ 
+
+/* Exercicio 3
+ */
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let soma = 0;
+let media= soma/numbers
+for( let i = 0; i< numbers.length; i++){
+    soma += numbers[i];
+ }
+ console.log(soma/numbers.length);
