93d551f199d80c602b56ef2a3d152a4dab714c97
Adan Filipe - Turma 22 - B <adan.cta@gmail.com>
2022-04-08 18:08:58 -0300

    trocando de exercicios

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -14,10 +14,25 @@ console.log(soma); */
 
 /* Exercicio 3
  */
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let media= soma/numbers
 for( let i = 0; i< numbers.length; i++){
     soma += numbers[i];
  }
- console.log(soma/numbers.length);
+ console.log(soma/numbers.length); */
+ 
+ /* Exercicio 4  */
+ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+ let soma = 0;
+ let media= soma/numbers
+ for( let i = 0; i< numbers.length; i++){
+     soma += numbers[i];
+  }
+  /* console.log(soma/numbers.length); */ 
+  if ((soma/numbers.length)> 20){
+      console.log("Valor maior que 20");
+  } else ((soma/numbers.length )< 20){
+      console.log("valor menor ou igual a 20");
+  }
+  
