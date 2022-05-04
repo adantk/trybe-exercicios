2cd98d54bdb885de931c58ab3241ad65c5a6c56f
Adan Filipe - Turma 22 - B <adan.cta@gmail.com>
2022-05-04 09:17:38 -0300

    Finalizado exercicio Tech-gallery

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicios dia 4.4.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicios dia 4.4.js
@@ -6,7 +6,7 @@ let info = {
   };
   console.log("Bem-vida,"+" "+ info.personagem);
  */
-/* Exercicio 2  */
+/* /* Exercicio 2/*  (Resolvido!!! VQV)
 let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
@@ -15,3 +15,14 @@ let info = {
   };
   console.log(info.recorrente);
 
+ */
+/* Exercicio 3 */
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
+  };
+  for (let key in info){
+  console.log(info[index]);
+  }
